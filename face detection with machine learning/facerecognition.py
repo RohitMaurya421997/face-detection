0a5bdd6bb822b4609c53f20f2881d828b2f310c6
Rohit Maurya <60098204+RohitMaurya421997@users.noreply.github.com>
2020-04-21 10:04:06 +0530

    Update and rename  facerecognition.py to facerecognition.py

--- face detection with machine learning/facerecognition.py
+++ face detection with machine learning/facerecognition.py
@@ -8,19 +8,13 @@ from PIL import Image, ImageDraw
 obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
+    obama_face_encoding]
+
 known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
-]
+    "Barack Obama" ]
 
 # Load an image with an unknown face
 unknown_image = face_recognition.load_image_file("two_people.jpg")
